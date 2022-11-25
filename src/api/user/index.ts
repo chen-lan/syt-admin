67062cdf173d6f8f81a9bc1b52a9252a45a3e192
森瀚 <10821174+senhan@user.noreply.gitee.com>
2022-11-25 09:14:40 +0000

    update src/api/user/index.ts.
    Signed-off-by: 森瀚 <10821174+senhan@user.noreply.gitee.com>

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -5,7 +5,7 @@ import type { GetInfoResponse, LoginResponse } from "./model/userTypes";
 export const reqLogin = (username: string, password: string) => {
   // <any, string>的第一个类型为any即可，实际不会用上
   // 第二个类型是返回值数据中data的类型，根据接口文档填写
-  return request.post<any, LoginResponse>("/admin/auth/index/login", {
+  return request.post<any, LoginResponse>("/admin/acl/index/login", {
     username,
     password,
   });
@@ -18,5 +18,5 @@ export const reqLogout = () => {
 
 // 查询用户信息
 export const reqGetInfo = () => {
-  return request.get<any, GetInfoResponse>("/admin/auth/index/info");
+  return request.get<any, GetInfoResponse>("/admin/acl/index/info");
 };
